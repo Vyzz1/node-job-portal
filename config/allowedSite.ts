ba7693adaa63c6a6a21318d740913c1e61da4337
VyHuynh <vykhanghuynh@gmail.com>
2024-09-11 23:30:58 +0700

    Refactor allowedSite.ts to update allowed site URL

--- config/allowedSite.ts
+++ config/allowedSite.ts
@@ -1,6 +1,1 @@
-export const allowedSite = [
-  "http://localhost:3000",
-  "http://localhost:3500",
-  "http://localhost:5173",
-  "http://localhost:3500",
-];
+export const allowedSite = ["https://visionary-seven.vercel.app"];
